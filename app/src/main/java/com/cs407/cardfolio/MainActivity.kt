aeae6f762df826cd59045b6233193ea3b9ffbebd
advillota <villota@wisc.edu>
2025-09-22 03:33:17 -0500

    ermmm in case mine laptop dies

--- app/src/main/java/com/cs407/cardfolio/MainActivity.kt
+++ app/src/main/java/com/cs407/cardfolio/MainActivity.kt
@@ -1,17 +1,55 @@
 package com.cs407.cardfolio
 
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.cardfolio.ui.theme.CardfolioTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +57,117 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CardfolioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.headlineLarge,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 24.dp)
                     )
+                    Cardfolio()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Cardfolio() {
+    var name by remember { mutableStateOf(value = "") }
+    var hobby by remember { mutableStateOf(value ="") }
+    var age by remember { mutableStateOf(value ="") }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all=16.dp)
+    ) {
+        ElevatedCard (
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            shape = MaterialTheme.shapes.extraLarge,
+            elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+            colors = CardDefaults.cardColors(  containerColor = MaterialTheme.colorScheme.surface)
+
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.thefigure),
+                    contentDescription = "pfp",
+                    modifier = Modifier
+                        .size(size = 84.dp)
+                        .clip(CircleShape)
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.outline,
+                            CircleShape
+                        )
+                )
+                Spacer(Modifier.width(width = 16.dp))
+
+                Column(Modifier.weight(weight = 1f)) {
+                    Text(
+                        text = if (name.isBlank()) stringResource(id = R.string.name) else name,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = if (hobby.isBlank()) stringResource(id = R.string.hobby) else hobby,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+                HorizontalDivider(color = MaterialTheme.colorScheme.outline)
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 16.dp ),
+                    verticalArrangement = Arrangement.spacedBy(space = 12.dp)
+                ){
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = { name = it },
+                        label = { Text(text = stringResource(id = R.string.card_name_label)) },
+                        leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) },
+                        modifier = Modifier.fillMaxWidth()
+
+                    )
+                    OutlinedTextField(
+                        value = hobby,
+                        onValueChange = { hobby = it },
+                        label = { Text(text=stringResource(id = R.string.card_hobby_label)) },
+                        leadingIcon = { Icon(Icons.Default.Favorite, contentDescription = null) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    OutlinedTextField(
+                        value = age,
+                        onValueChange = { input ->
+                            if (input.all { it.isDigit() }) {
+                                age = input
+                            }
+                        },
+                        label = { Text(text=stringResource(id = R.string.card_age_label)) },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        leadingIcon = { Icon(Icons.Default.DateRange, contentDescription = null) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CardfolioPreview() {
     CardfolioTheme {
-        Greeting("Android")
+        Cardfolio()
     }
 }
